a42074d88ac403ecb7330bfd48eee82a5ae8e25c
wkdewey <wkdewey@gmail.com>
2020-07-08 08:02:24 -0400

    add user_id to places table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200708113502) do
+ActiveRecord::Schema.define(version: 20200708114210) do
 
   create_table "places", force: :cascade do |t|
     t.string   "country"
     t.string   "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
