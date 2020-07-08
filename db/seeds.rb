a86f2a6d403430fcb394ce21e93ed0967ca7816d
wkdewey <wkdewey@gmail.com>
2020-07-08 08:11:50 -0400

    add faker gem and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ require "faker"
   user_hometown = Faker::Address.city
   user_password = Faker::Alphanumeric.alphanumeric(number: 10)
   place_name = Faker::Address.community
-  place_city = Address.city
-  place_country = Address.country
+  place_city = Faker::Address.city
+  place_country = Faker::Address.country
   place_user_id = rand(1..10)
   User.create(name: user_name, hometown: user_hometown, password: user_password)
 end
