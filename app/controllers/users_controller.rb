afac160db2580a9a5f7f0d5f75194756fe4311dd
wkdewey <wkdewey@gmail.com>
2020-07-08 13:33:21 -0400

    create delete route for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,9 @@ class UsersController < ApplicationController
 
   # DELETE: /users/5/delete
   delete "/users/:id/delete" do
-    redirect "/users"
+    user = User.find_by(id: params[:id])
+    user.destroy
+    session.clear
+    redirect "/"
   end
 end
