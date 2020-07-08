a88d5de0f92c94c822ccd83b46887f5479ce4a8f
wkdewey <wkdewey@gmail.com>
2020-07-08 08:05:51 -0400

    complete seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,11 @@ require "faker"
 
 10.times do
   user_name = Faker::Name.name
-  user_place = Faker::Address.city
+  user_hometown = Faker::Address.city
   user_password = Faker::Alphanumeric.alphanumeric(number: 10)
   place_name = Faker::Address.community
   place_city = Address.city
   place_country = Address.country
+  place_user_id = rand(1..10)
+  User.create(name: user_name, hometown: user_hometown, password: user_password)
 end
