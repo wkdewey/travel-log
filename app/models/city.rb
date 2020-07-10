46d6855385b4a7be9f896a717421df476133ebb0
wkdewey <wkdewey@gmail.com>
2020-07-10 17:10:27 -0400

    add users to city and country models

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,6 @@
 class City < ActiveRecord::Base
   belongs_to :country
   has_many :places
+  has_many :users, through: :places
   validates :name, :country_id, presence: true
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,6 @@
 class Country < ActiveRecord::Base
   has_many :cities
   has_many :places, through: :cities
+  has_many :users, through: :places
   validates :name, presence: true
 end
