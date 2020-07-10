1c9075863691db2f14d1915dbf550bb70591fe24
wkdewey <wkdewey@gmail.com>
2020-07-10 16:51:48 -0400

    add views for countries and cities

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,37 +1,14 @@
 class CountriesController < ApplicationController
 
-  # GET: /countries
+  
   get "/countries" do
-    erb :"/countries/index.html"
+    @countries = Country.all
+    erb :"/countries/index"
   end
 
-  # GET: /countries/new
-  get "/countries/new" do
-    erb :"/countries/new.html"
-  end
-
-  # POST: /countries
-  post "/countries" do
-    redirect "/countries"
-  end
-
-  # GET: /countries/5
+  
   get "/countries/:id" do
-    erb :"/countries/show.html"
-  end
-
-  # GET: /countries/5/edit
-  get "/countries/:id/edit" do
-    erb :"/countries/edit.html"
-  end
-
-  # PATCH: /countries/5
-  patch "/countries/:id" do
-    redirect "/countries/:id"
-  end
-
-  # DELETE: /countries/5/delete
-  delete "/countries/:id/delete" do
-    redirect "/countries"
+    @country = Country.find_by_id(params[:id])
+    erb :"/countries/show"
   end
 end
