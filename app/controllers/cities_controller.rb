a8dd607b14995100e36ea52a780384a081c31a4c
wkdewey <wkdewey@gmail.com>
2020-07-10 16:34:49 -0400

    create show route and view for individual cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -6,33 +6,10 @@ class CitiesController < ApplicationController
     erb :"/cities/index"
   end
 
-  # GET: /cities/new
-  get "/cities/new" do
-    erb :"/cities/new.html"
-  end
-
-  # POST: /cities
-  post "/cities" do
-    redirect "/cities"
-  end
-
   # GET: /cities/5
   get "/cities/:id" do
-    erb :"/cities/show.html"
+    @city = City.find_by_id(params[:id])
+    erb :"/cities/show"
   end
 
-  # GET: /cities/5/edit
-  get "/cities/:id/edit" do
-    erb :"/cities/edit.html"
-  end
-
-  # PATCH: /cities/5
-  patch "/cities/:id" do
-    redirect "/cities/:id"
-  end
-
-  # DELETE: /cities/5/delete
-  delete "/cities/:id/delete" do
-    redirect "/cities"
-  end
 end
