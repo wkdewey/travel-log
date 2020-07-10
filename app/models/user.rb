826f87e2386e94882e3b9655dd92171fee87399b
wkdewey <wkdewey@gmail.com>
2020-07-10 19:23:05 -0400

    add countries and cities to user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :user_places
   has_many :places, through: :user_places
+  has_many :cities, through: :places
+  has_many :countries, through: :cities 
   validates :name, uniqueness: true
   validates :name, :password_digest, presence: true
 end
